3539bfdf577d37ae61b54fe7a4fd8d293b0ae9ff
John Cevallos <joancemac@gmail.com>
2022-04-21 15:05:53 -0500

    prueba de concepto arrow functions

--- 01practicaIntroduccion2/clases1.js
+++ 01practicaIntroduccion2/clases1.js
@@ -6,21 +6,35 @@ const persona={
         lat:234.234234,
         lng:34.3453453,
     },
-    getNombreCompleto: function(){
+    getNombreCompleto: ()=>{
         return `${this.nombre} ${this.apellido}`
-    }
+    },
+    getNombreCompleto2: function(){
+        return `${this.nombre} ${this.apellido}`
+    },
+    
 }
 
-console.log(persona.getNombreCompleto())
+ this.nombre='john';
+ this.apellido='cevallos';
+// console.log(persona.getNombreCompleto())
 
 
-// function mostrarDatos({ nombre, apellido, geolocalizacion: { lat, lng }, getNombreCompleto })
-// {
-//     console.log(nombre)
-//     console.log(apellido)
-//     console.log(lat)
-//     console.log(lng)
-//     console.log(getNombreCompleto())
-// }
+//  console.log(persona.getNombreCompleto())
+//  console.log(persona.getNombreCompleto2())
 
-// mostrarDatos(persona)
+
+function mostrarDatos({ nombre, apellido, geolocalizacion: { lat, lng }, getNombreCompleto, getNombreCompleto2 })
+{
+    this.nombre='prueba'
+    this.apellido='prueba2'
+    // console.log(nombre)
+    // console.log(apellido)
+    // console.log(lat)
+    // console.log(lng)
+    console.log(getNombreCompleto())
+    console.log(getNombreCompleto2())
+
+}
+
+ mostrarDatos(persona)
