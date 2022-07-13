0dfff94262e418f36280430f289ef4f4c873265d
John Cevallos <joancemac@gmail.com>
2022-07-13 11:16:23 -0500

    Terminamos SSR

---
+++ 14SSR/crudssr/routes/index.js
@@ -0,0 +1,30 @@
+const axios  = require('axios');
+var express = require('express');
+var router = express.Router();
+
+
+const httpAxios= axios.create({
+  baseURL:`http://localhost:2500/v1/sextoa/api/`
+})
+
+router.get('/', function(req, res, next) {
+  httpAxios.get(`productos`).then(respuesta=>{
+    // console.log(respuesta.data.productos);
+    res.render('index', { productos: respuesta.data.productos });
+  })
+});
+router.get('/producto/nuevo',(req,res,next)=>{
+  res.render('productoForm',{})
+})
+router.get('/producto/modificar/:id',(req,res,next)=>{
+  httpAxios.get(`productos/${req.params.id}`).then(respuesta=>{
+    res.render('productoForm', { producto :respuesta.data })
+  })
+})
+router.get('/producto/eliminar/:id',(req,res,next)=>{
+  httpAxios.delete(`productos/${req.params.id}`).then(respuesta=>{
+    res.redirect('/')
+  })
+})
+
+module.exports = router;
